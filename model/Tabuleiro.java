e6c1c3e7104a37c59d2399dde603e9cdbab9a7ac
rodi38 <rodrigorochaed1@gmail.com>
2022-12-26 20:12:18 -0300

    Adicionando o metodo de shooting

--- model/Tabuleiro.java
+++ model/Tabuleiro.java
@@ -4,35 +4,40 @@ import util.Util;
 import view.Print;
 
 public class Tabuleiro {
+    public String[][] getTabuleiro() {
+        return tabuleiro;
+    }
+
     private String[][] tabuleiro = new String[10][10];
 
-    Tabuleiro(){
-        posicionarNavios(5);
+    public void setShoot(int line, int column, String shoot) {
+        this.tabuleiro[line][column] = shoot;
+    }
+
+    public Tabuleiro() {
         preencherTabuleiro();
-        Print.imprimirTabuleiro(tabuleiro);
     }
 
     void posicionarNavios(int quantidadeNavios) {
         for (int i = 0; i < quantidadeNavios; i++) {
-            int linhaAleatoria = Util.intAleatorio(0,tabuleiro.length);
-            int colunaAleatoria = Util.intAleatorio(0,tabuleiro.length);
-            if (tabuleiro[linhaAleatoria][colunaAleatoria] == null){
+            int linhaAleatoria = Util.intAleatorio(0, tabuleiro.length);
+            int colunaAleatoria = Util.intAleatorio(0, tabuleiro.length);
+            if (tabuleiro[linhaAleatoria][colunaAleatoria] == null) {
                 tabuleiro[linhaAleatoria][colunaAleatoria] = "+";
             } else {
-                i=i-1;
+                i = i - 1;
             }
         }
     }
-    
+
     void preencherTabuleiro() {
         for (int i = 0; i < tabuleiro.length; i++) {
             for (int j = 0; j < tabuleiro.length; j++) {
-                if(tabuleiro[i][j] != "+")
-                tabuleiro[i][j]="~";
+                if (tabuleiro[i][j] != "+")
+                    tabuleiro[i][j] = "~";
             }
         }
     }
-    
 
 
 }
